79fb4d95317f3c541307261a384fb9cc84e63843
youssef24hilali <y.elhilali24@gmail.com>
2023-06-01 11:22:00 +0100

    pms-backend-commit

--- src/main/java/com/pmso/product_management_system_original/dataaccess/entities/Category.java
+++ src/main/java/com/pmso/product_management_system_original/dataaccess/entities/Category.java
@@ -1,5 +1,6 @@
 package com.pmso.product_management_system_original.dataaccess.entities;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -27,9 +28,11 @@ public class Category {
     private String slug;
 
     @Column(name = "date_creation")
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
     private Date dateCreation;
 
     @Column(name = "date_modification")
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
     private Date dateModification;
 
     private Boolean supprimer;
